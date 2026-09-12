d32bec4842c643a800fc2005808e2083341a11a5
Eduard Šimon <eda.simon14@gmail.com>
2026-09-12 09:58:28 +0200

    add matmul, transpose and begin implementing DCT

--- load_image.cpp
+++ load_image.cpp
@@ -1,11 +1,58 @@
 #include <bits/stdc++.h>
 #include <cassert>
+#include <array>
+using Matrix8 = std::array<std::array<double, 8>, 8>;
+
 using namespace std;
 
 struct Image {
     int width = 0, height = 0;
     vector<int> pixels;
 };
+
+// Cosine bases for DCT
+Matrix8 A = {{
+      {0.353553,  0.353553, 0.353553, 0.353553, 0.353553, 0.353553, 0.353553, 0.353553},
+      {0.490393, 0.415735, 0.277785, 0.097545,-0.097545,-0.277785,-0.415735,-0.490393},
+      {0.461940, 0.191342,-0.191342,-0.461940,-0.461940,-0.191342, 0.191342, 0.461940},
+      {0.415735, -0.097545,-0.490393,-0.277785, 0.277785, 0.490393, 0.097545,-0.415735},
+      {0.353553, -0.353553,-0.353553, 0.353553, 0.353553,-0.353553,-0.353553, 0.353553},
+      {0.277785, -0.490393, 0.097545, 0.415735,-0.415735,-0.097545, 0.490393,-0.277785},
+      {0.191342, -0.461940, 0.461940,-0.191342,-0.191342, 0.461940,-0.461940, 0.191342},
+      {0.097545, -0.277785, 0.415735,-0.490393, 0.490393,-0.415735, 0.277785,-0.097545}
+    }};
+
+Matrix8 matmul(const Matrix8& A, const Matrix8& B) {
+    Matrix8 C{};
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            for (int k = 0; k < 8; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    return C;
+}
+
+Matrix8 transpose(const Matrix8& M) {
+    Matrix8 C{};
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            C[j][i] = M[i][j];
+        }
+    }
+    return C;
+}
+
+Matrix8 dct8x8(const double in[8][8]) {
+    Matrix8 out;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            out[i][j] = in[i][j] - 128;
+        }
+    }
+    return matmul(matmul(A, out), transpose(A));
+}
 
 Image processBlocks(const Image& input) {
     vector<int> num_of_blocks(2);
@@ -49,11 +96,8 @@ void pixels2img(const char* out_loc, const Image& input) {
     }
 }
 
+
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        cerr << "use: " << argv[0] << " in.pgm out.pgm\n";
-        return 1;
-    }
 
     ifstream in(argv[1]);
     if (!in) { cerr << "Cannot open in.pgm" << "\n"; return 1; }
